ea882364f7bdd3190c12873251bb0cef2248985a
Lucas Carioca <lucascarioca97@gmail.com>
2022-08-09 19:53:10 -0300

    Setup App Structure, added Retrofit and Hilt Classes

---
+++ app/src/main/java/com/example/triviaapp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.triviaapp.utils
+
+object Constants {
+
+    //raw.githubusercontent.com/itmmckernan/triviaJSON/master/world.json
+    const val BASE_URL = "https://raw.githubusercontent.com/itmmckernan/triviaJSON/master/"
+}
